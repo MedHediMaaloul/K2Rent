3042cb97d2f1c5b0cf27baf3cd7bad6f4505cee6
unknown <malek.moslahh@gmail.com>
2023-02-16 17:57:38 +0100

    create planning contrat

--- Gestion_location/inc/footer.php
+++ Gestion_location/inc/footer.php
@@ -33,7 +33,8 @@
 <script src="https://unpkg.com/feather-icons"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> 
-
+<!--Calendar-->
+<script src="assets/js/index.global.js"></script>
 <script>
 	feather.replace()
 </script>
